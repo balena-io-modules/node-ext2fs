00d3765ec98dc4636ffb259c68c97bc587e92a6a
Alexis Svinartchouk alexis@resin.io <alexis@resin.io>
2017-04-12 13:30:53 +0200

    Update resin-file-dsik to 0.0.8
    Change-Type: patch

--- test/index.js
+++ test/index.js
@@ -11,22 +11,26 @@ const EXT4_PATH = pathModule.join(__dirname, 'fixtures/ext4.img');
 describe('ext2fs', function() {
 	describe('mount', function() {
 		it('ext2', function() {
-			const disk = new filedisk.FileDisk(EXT2_PATH, ext2fs.constants.FILE_DISK_MAPPING);
-			return ext2fs.mountAsync(disk);
+			return Promise.using(filedisk.openFile(EXT2_PATH, 'r'), function(fd) {
+				return ext2fs.mountAsync(new filedisk.FileDisk(fd));
+			})
 		});
 		it('ext3', function() {
-			const disk = new filedisk.FileDisk(EXT3_PATH, ext2fs.constants.FILE_DISK_MAPPING);
-			return ext2fs.mountAsync(disk);
+			return Promise.using(filedisk.openFile(EXT3_PATH, 'r'), function(fd) {
+				return ext2fs.mountAsync(new filedisk.FileDisk(fd));
+			});
 		});
 		it('ext4', function() {
-			const disk = new filedisk.FileDisk(EXT4_PATH, ext2fs.constants.FILE_DISK_MAPPING);
-			return ext2fs.mountAsync(disk);
+			return Promise.using(filedisk.openFile(EXT4_PATH, 'r'), function(fd) {
+				return ext2fs.mountAsync(new filedisk.FileDisk(fd));
+			})
 		});
 	});
 	describe('trim', function() {
 		it('ext4', function() {
-			const disk = new filedisk.FileDisk(EXT4_PATH, ext2fs.constants.FILE_DISK_MAPPING);
-			return ext2fs.mountAsync(disk)
+			return Promise.using(filedisk.openFile(EXT4_PATH, 'r'), function(fd) {
+				return ext2fs.mountAsync(new filedisk.FileDisk(fd));
+			})
 			.then(function(fs) {
 				return ext2fs.trimAsync(fs);
 			});
