4e854e401f47d626de9f686812330757a31648ef
Alexis Svinartchouk <alexis@resin.io>
2017-05-24 19:40:26 +0200

    add missing headers in ext2fs.h factorize node_ext2fs.cc factorize tests test creating a file in a folder that does not exist

--- test/index.js
+++ test/index.js
@@ -14,14 +14,39 @@ const IMAGES = {
 	'ext4': 'ext4.img'
 };
 
+function humanFileMode(fs, stats) {
+	const result = [];
+	result.push(stats.isDirectory() ? 'd' : '-');
+	let constant, enabled;
+	for (let actor of ['USR', 'GRP', 'OTH']) {
+		for (let action of ['R', 'W', 'X']) {
+			constant = fs.constants[`S_I${action}${actor}`];
+			enabled = ((stats.mode & constant) !== 0);
+			result.push(enabled ? action.toLowerCase() : '-');
+		}
+	}
+	return result.join('');
+}
+
 function testOnAllDisks(fn) {
 	return Object.keys(IMAGES).forEach(function(name) {
 		it(name, function(){
 			const path = pathModule.join(__dirname, 'fixtures', IMAGES[name]);
 			return Promise.using(filedisk.openFile(path, 'r'), function(fd) {
-				const options = { readOnly: true, recordWrites: true };
-				const disk = new filedisk.FileDisk(fd, options);
-				return fn(disk);
+				return fn(new filedisk.FileDisk(fd, true, true));
+			});
+		});
+	});
+}
+
+function testOnAllDisksMount(fn) {
+	return testOnAllDisks(function(disk) {
+		return ext2fs.mountAsync(disk)
+		.then(function(fs) {
+			fs = Promise.promisifyAll(fs, { multiArgs: true });
+			return fn(fs)
+			.then(function() {
+				return ext2fs.umountAsync(fs);
 			});
 		});
 	});
@@ -31,96 +56,168 @@ describe('ext2fs', function() {
 	after(ext2fs.close);
 
 	describe('mount, open, read, close, umount', function() {
-		testOnAllDisks(function(disk) {
-			return ext2fs.mountAsync(disk)
-			.then(function(fs){
-				fs = Promise.promisifyAll(fs, { multiArgs: true });
-				const buf = Buffer.allocUnsafe(4);
-				return fs.openAsync('/1', 'r')
-				.then(function(fd) {
-					return fs.readAsync(fd, buf, 0, 4, 0)
-					.spread(function(bytesRead, buf) {
-						assert.strictEqual(bytesRead, 4);
-						assert.strictEqual(buf.toString(), 'one\n');
-						return fs.closeAsync(fd);
-					});
-				})
-				.then(function() {
-					return ext2fs.umountAsync(fs);
+		testOnAllDisksMount(function(fs) {
+			const buf = Buffer.allocUnsafe(4);
+			return fs.openAsync('/1', 'r')
+			.then(function(fd) {
+				return fs.readAsync(fd, buf, 0, 4, 0)
+				.spread(function(bytesRead, buf) {
+					assert.strictEqual(bytesRead, 4);
+					assert.strictEqual(buf.toString(), 'one\n');
+					return fs.closeAsync(fd);
 				});
 			});
 		});
 	});
 
-	describe('mount, create, write, read, close, umount', function() {
-		testOnAllDisks(function(disk) {
-			return ext2fs.mountAsync(disk)
-			.then(function(fs){
-				fs = Promise.promisifyAll(fs, { multiArgs: true });
-				const buf = Buffer.from('six\n');
-				return fs.openAsync('/6', 'r+')
-				.then(function(fd) {
-					return fs.writeAsync(fd, buf, 0, buf.length, 0)
-					.spread(function(bytesWritten, buf) {
-						assert.strictEqual(bytesWritten, buf.length);
-						assert.strictEqual(buf.toString(), 'six\n');
-						buf.fill(0);
-						return fs.readAsync(fd, buf, 0, 1024, 0);
-					})
-					.spread(function(bytesRead, buf) {
-						assert.strictEqual(bytesRead, 4);
-						assert.strictEqual(buf.toString(), 'six\n');
-						return fs.closeAsync(fd);
-					});
-				})
-				.then(function() {
-					return ext2fs.umountAsync(fs);
+	describe('mount, open, fstat, close, umount', function() {
+		testOnAllDisksMount(function(fs) {
+			return fs.openAsync('/2', fs.O_RDONLY | fs.O_NOATIME)
+			.then(function(fd) {
+				return fs.fstatAsync(fd)
+				.spread(function(stats) {
+					assert.strictEqual(stats.dev, 0);
+					assert.strictEqual(stats.mode, 33188);
+					assert.strictEqual(stats.nlink, 1);
+					assert.strictEqual(stats.uid, 1000);
+					assert.strictEqual(stats.gid, 1000);
+					assert.strictEqual(stats.rdev, 0);
+					assert.strictEqual(stats.blksize, 1024);
+					assert.strictEqual(stats.size, 4);
+					assert.strictEqual(stats.blocks, 2);
+					assert.strictEqual(
+						stats.atime.getTime(),
+						(new Date('2017-05-23T18:56:45.000Z')).getTime()
+					);
+					assert.strictEqual(
+						stats.mtime.getTime(),
+						(new Date('2017-05-22T13:02:28.000Z')).getTime()
+					);
+					assert.strictEqual(
+						stats.ctime.getTime(),
+						(new Date('2017-05-23T18:56:47.000Z')).getTime()
+					);
+					assert.strictEqual(
+						stats.birthtime.getTime(),
+						(new Date('2017-05-23T18:56:47.000Z')).getTime()
+					);
+					return fs.closeAsync(fd);
 				});
+			});
+		});
+	});
+
+	describe('mount, create, write, fstat, close, open, fstat, read, close, umount', function() {
+		const path = '/6';
+		const content = 'six\n';
+		testOnAllDisksMount(function(fs) {
+			let statsBeforeClose;
+			const buf = Buffer.from(content);
+			return fs.openAsync(path, 'w+', 0o777)
+			.then(function(fd) {
+				return fs.writeAsync(fd, buf, 0, buf.length, 0)
+				.spread(function(bytesWritten, buf) {
+					assert.strictEqual(bytesWritten, buf.length);
+					assert.strictEqual(buf.toString(), content);
+					return fs.fstatAsync(fd);
+				})
+				.spread(function(stats) {
+					statsBeforeClose = stats;
+					return fs.closeAsync(fd);
+				});
+			})
+			.then(function() {
+				return fs.openAsync(path, 'r');
+			})
+			.spread(function(fd) {
+				return fs.fstatAsync(fd)
+				.spread(function(stats) {
+					// compare the 2 Stats objects
+					let value, otherValue;
+					for (let key of Object.keys(statsBeforeClose)) {
+						value = statsBeforeClose[key]
+						otherValue = stats[key]
+						if (value instanceof Date) {
+							value = value.getTime();
+							otherValue = otherValue.getTime();
+						}
+						assert.strictEqual(value, otherValue);
+					}
+					assert(stats.isFile());
+					assert.strictEqual(stats.dev, 0);
+					assert.strictEqual(stats.nlink, 1);
+					assert.strictEqual(stats.uid, 0);
+					assert.strictEqual(stats.gid, 0);
+					assert.strictEqual(stats.rdev, 0);
+					assert.strictEqual(stats.blocks, 2);
+					assert.strictEqual(stats.size, content.length);
+					assert.strictEqual(humanFileMode(fs, stats), '-rwxrwxrwx');
+					const now = Date.now();
+					assert(now - stats.atime.getTime() < 3000);
+					assert(now - stats.ctime.getTime() < 3000);
+					assert(now - stats.mtime.getTime() < 3000);
+					assert(now - stats.birthtime.getTime() < 3000);
+					buf.fill(0);
+					return fs.readAsync(fd, buf, 0, 1024, 0);
+				})
+				.spread(function(bytesRead, buf) {
+					assert.strictEqual(bytesRead, content.length);
+					assert.strictEqual(buf.toString(), content);
+					return fs.closeAsync(fd);
+				})
 			});
 		});
 	});
 
 	describe('mount, readFile, umount', function() {
-		testOnAllDisks(function(disk) {
-			return ext2fs.mountAsync(disk)
-			.then(function(fs){
-				fs = Promise.promisifyAll(fs, { multiArgs: true });
-				return fs.readFileAsync('/1', 'utf8')
-				.spread(function(data) {
-					assert.strictEqual(data, 'one\n');
-					return ext2fs.umountAsync(fs);
-				});
+		testOnAllDisksMount(function(fs) {
+			return fs.readFileAsync('/1', 'utf8')
+			.spread(function(data) {
+				assert.strictEqual(data, 'one\n');
 			});
 		});
 	});
 
 	describe('mount, readdir, umount', function() {
-		testOnAllDisks(function(disk) {
-			return ext2fs.mountAsync(disk)
-			.then(function(fs){
-				fs = Promise.promisifyAll(fs, { multiArgs: true });
-				return fs.readdirAsync('/')
-				.spread(function(filenames) {
-					filenames.sort();
-					assert.deepEqual(
-						filenames,
-						[ '1', '2', '3', '4', '5', 'lost+found' ]
-					);
-					return ext2fs.umountAsync(fs);
-				});
+		testOnAllDisksMount(function(fs) {
+			return fs.readdirAsync('/')
+			.spread(function(filenames) {
+				filenames.sort();
+				assert.deepEqual(
+					filenames,
+					[ '1', '2', '3', '4', '5', 'lost+found' ]
+				);
+			});
+		});
+	});
+
+	describe('open non existent file for reading', function() {
+		testOnAllDisksMount(function(fs) {
+			return fs.openAsync('/7', 'r')
+			.spread(function(fd) {
+				assert(false);
+			})
+			.catch(function(err) {
+				assert.strictEqual(err.errno, 2);
+				assert.strictEqual(err.code, 'ENOENT');
+			});
+		});
+	});
+
+	describe('create file in non existent folder', function() {
+		testOnAllDisksMount(function(fs) {
+			return fs.openAsync('/7/8', 'w')
+			.spread(function(fd) {
+				assert(false);
+			})
+			.catch(function(err) {
+				assert.strictEqual(err.errno, 20);
+				assert.strictEqual(err.code, 'ENOTDIR');
 			});
 		});
 	});
 
 	describe('trim', function() {
-		testOnAllDisks(function(disk) {
-			return ext2fs.mountAsync(disk)
-			.then(function(fs) {
-				return ext2fs.trimAsync(fs)
-				.then(function(){
-					return ext2fs.umountAsync(fs);
-				});
-			});
-		});
+		testOnAllDisksMount(ext2fs.trimAsync);
 	});
 });
