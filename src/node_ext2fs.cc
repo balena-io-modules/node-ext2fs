6a833a04d042643391ad6a2564ba372e71ff2cf0
Tim Perry <pimterry@gmail.com>
2017-11-16 16:15:34 +0100

    Add clock_gettime implementation so this works on OSX < 10.11
    Change-Type: patch

--- src/node_ext2fs.cc
+++ src/node_ext2fs.cc
@@ -386,13 +386,30 @@ class CloseWorker : public AsyncWorker {
 X_NAN_METHOD(close, CloseWorker, 3);
 
 #ifdef _WIN32
+// Windows does not include CLOCK_REALTIME or clock_gettime
 #define CLOCK_REALTIME 0
-int clock_gettime(int, struct timespec *spec)      //C-file part
-{  __int64 wintime; GetSystemTimeAsFileTime((FILETIME*)&wintime);
-   wintime      -=116444736000000000i64;  //1jan1601 to 1jan1970
-   spec->tv_sec  =wintime / 10000000i64;           //seconds
-   spec->tv_nsec =wintime % 10000000i64 *100;      //nano-seconds
-   return 0;
+int clock_gettime(int, struct timespec *spec) {
+	__int64 wintime; GetSystemTimeAsFileTime((FILETIME*)&wintime);
+	wintime      -= 116444736000000000i64;  //1jan1601 to 1jan1970
+	spec->tv_sec  = wintime / 10000000i64;           //seconds
+	spec->tv_nsec = wintime % 10000000i64 *100;      //nano-seconds
+	return 0;
+}
+#endif
+
+#if defined(__MACH__) && !defined(CLOCK_REALTIME)
+// clock_gettime is not implemented on older versions of OS X (< 10.12).
+// If implemented, CLOCK_REALTIME will have already been defined.
+
+#include <sys/time.h>
+#define CLOCK_REALTIME 0
+int clock_gettime(int, struct timespec* spec) {
+    struct timeval now;
+    int rv = gettimeofday(&now, NULL);
+    if (rv) return rv;
+    spec->tv_sec  = now.tv_sec;
+    spec->tv_nsec = now.tv_usec * 1000;
+    return 0;
 }
 #endif
 
